3670e5382fc2ff9fd52862815ea8a619cf1d01dc
clixiang <lixiang761@126.com>
2018-08-09 17:19:01 +0800

    Site updated: 2018-08-09 17:18:59

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 08 2018 11:08:47 GMT+0800 (CST)
+// build time:Thu Aug 09 2018 17:17:58 GMT+0800 (CST)
 $(document).ready(function(){var t=$(".sidebar-inner");function e(){return $(".header-inner").height()+CONFIG.sidebar.offset}function i(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function n(){var n=e();var a=i();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(n+f<o){t.affix({offset:{top:n-CONFIG.sidebar.offset,bottom:a}})}r(n).css({"margin-left":"initial"})}function a(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");n()}function f(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){a()}})}n();f()});
 //rebuild by neat 
